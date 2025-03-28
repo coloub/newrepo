6caf1ee38ebbe49974428a13c0eda064d933e5a0
coloub <jose1252jose@gmail.com>
2025-03-28 13:17:19 -0400

    #

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -4,7 +4,7 @@ const router = new express.Router()
 const invController = require("../controllers/invController")
 
 // Ruta para la vista de gestión de inventario
-router.get("/", invController.showManagementView);
+router.get("/inv", invController.showManagementView);
 
 // Ruta para la vista de agregar clasificación
 router.get("/add-classification", invController.showAddClassificationView);
